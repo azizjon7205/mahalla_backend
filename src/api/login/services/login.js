0a4bf70c0b8e24509455c745d656a9f507b6c11f
azizjon <azizjonsheronov2607@gmail.com>
2023-08-05 18:17:58 +0500

    Login api changes

--- src/api/login/services/login.js
+++ src/api/login/services/login.js
@@ -9,26 +9,40 @@ module.exports = {
       try {
         const { phone_number, password, fcm_token } = ctx.request.body;
 
-        // Find the worker with the provided phone_number and password
-        const worker = await strapi.query('xodims').findOne({
-            phone_number,
-            password,
+        const entries = await strapi.entityService.findMany(
+            "api::xodim.xodim",
+            {
+              fields: ["id", "name", "phone_number", "password", "fcm_token"],
+              //populate: {
+              //  author: {
+              //    fields: ["name", "email"],
+              //  },
+              //  category: {
+              //    fields: ["name"],
+              //  },
+              //},
+            }
+          );
+
+        entries.array.forEach(async element => {
+            if(element.phone_number == phone_number && element.password == password){
+                const entry = await strapi.entityService.update('api::xodim.xodim', element.id, {
+                    data: {
+                      fcm_token: fcm_token,
+                    },
+                  });
+                return {
+                    success: true,
+                    message: 'Login successful.',
+                  };
+            }
         });
   
-        if (worker) {
-            // Update the worker's fcm_token
-            await strapi.query('xodims').update({ id: worker.id }, { fcm_token });
-      
-            return {
-              success: true,
-              message: 'Login successful.',
-            };
-          } else {
-            return {
-              success: false,
-              message: 'Invalid credentials.',
-            };
-          }
+        return {
+            success: false,
+            message: 'Invalid credentials.',
+          };
+
       } catch (err) {
         return {
         success: false,
@@ -38,4 +52,7 @@ module.exports = {
       }
     },
 };
+  
+
+
   
