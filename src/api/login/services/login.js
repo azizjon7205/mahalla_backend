dbe3b11bf1d39efbaa23cac56b25473eba459750
azizjon <azizjonsheronov2607@gmail.com>
2023-08-05 18:04:49 +0500

    Login api changes

--- src/api/login/services/login.js
+++ src/api/login/services/login.js
@@ -10,14 +10,14 @@ module.exports = {
         const { phone_number, password, fcm_token } = ctx.request.body;
 
         // Find the worker with the provided phone_number and password
-        const worker = await strapi.query('xodim').findOne({
+        const worker = await strapi.query('xodims').findOne({
             phone_number,
             password,
         });
   
         if (worker) {
             // Update the worker's fcm_token
-            await strapi.query('xodim').update({ id: worker.id }, { fcm_token });
+            await strapi.query('xodims').update({ id: worker.id }, { fcm_token });
       
             return {
               success: true,
